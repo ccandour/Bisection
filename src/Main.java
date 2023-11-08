46e3cb393f41a64f9253357b5fc88fbb5791490c
crazy <krzyzaczek101@gmail.com>
2023-11-08 10:15:31 +0100

    feat: added code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
+    int MAX_STEPS;
+    int TOLERANCE;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 1;
+        int
+        while ()
+
+        /*
+        N ← 1
+        while N ≤ NMAX do // limit iterations to prevent infinite loop
+            c ← (a + b)/2 // new midpoint
+        if f(c) = 0 or (b – a)/2 < TOL then // solution found
+        Output(c)
+        Stop
+        end if
+        N ← N + 1 // increment step counter
+        if sign(f(c)) = sign(f(a)) then a ← c else b ← c // new interval
+        end while
+        Output("Method failed.") // max number of steps exceeded
+        */
+
     }
 }
